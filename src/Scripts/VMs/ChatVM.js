bb2b9741159a6b56ccc1f2a4d8995e6246f9cedc
Maja Marchewka <marchewka.maja@gmail.com>
2016-04-28 14:43:16 +0200

    Skończona lista kontaków

--- src/Scripts/VMs/ChatVM.js
+++ src/Scripts/VMs/ChatVM.js
@@ -2,22 +2,31 @@
     var self = this;
 
     //observables
-    self.Chanels = ko.observableArray([]);
+    self.ContactChannels = ko.observableArray([]);
+    self.GroupChannels = ko.observableArray([]);
     self.messageToAdd = ko.observable("");
-    self.SelectedChanelName = ko.observable("");;
-
+    self.SelectedChanelName = ko.observable("");
+    self.LoggedUser = {
+        AvatarUri: ko.observable(""),
+        name: ko.observable(""),
+        email: ko.observable("")
+    };
 
     //computed
-    self.SelectedChanel = ko.computed(function() {
-        var selectedChanel = ko.utils.arrayFirst(self.Chanels(), function(chanel) {
+    self.SelectedChanel = ko.computed(function () {
+        var selectedChanel = ko.utils.arrayFirst(self.ContactChannels(), function (chanel) {
             return chanel.Name() === self.SelectedChanelName();
         });
-
+        if (!selectedChanel) {
+            selectedChanel = ko.utils.arrayFirst(self.GroupChannels(), function (chanel) {
+                return chanel.Name() === self.SelectedChanelName();
+            });
+        }
         return selectedChanel;
     });
 
     //functions
-    self.sendMessage = function() {
+    self.sendMessage = function () {
         if (self.messageToAdd() != "") {
             var message = new MesseageVM({ Content: self.messageToAdd() });
             self.SelectedChanel().Messeges.push(message);
@@ -31,12 +40,31 @@
 
 
     //test initialize data
-    var chanel1 = new ChanelVM({ Name: "Team A" });
-    var chanel2 = new ChanelVM({ Name: "Bash" });
-    var chanel3 = new ChanelVM({ Name: "Spam here" });
-    self.Chanels.push(chanel1);
-    self.Chanels.push(chanel2);
-    self.Chanels.push(chanel3);
-    self.SelectedChanelName("Team A");
+    var chanel1 = new ChanelVM({ Name: "Kontakt 1", AvatarUri: "Content/Images/sample.jpg", IsOffline: false, AllRead: false });
+    var chanel2 = new ChanelVM({ Name: "Kontakt 2", AvatarUri: "Content/Images/sample.jpg", IsOffline: false, AllRead: true });
+    var chanel3 = new ChanelVM({ Name: "Kontakt 3", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: false });
+    var chanel4 = new ChanelVM({ Name: "Kontakt 4", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: true });
+    var chanel5 = new ChanelVM({ Name: "Kontakt 5", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: true });
+    self.ContactChannels.push(chanel1);
+    self.ContactChannels.push(chanel2);
+    self.ContactChannels.push(chanel3);
+    self.ContactChannels.push(chanel4);
+    self.ContactChannels.push(chanel5);
 
+    var chanel6 = new ChanelVM({ Name: "Grupa 1", AvatarUri: "Content/Images/sample.jpg", IsOffline: false, AllRead: false });
+    var chanel7 = new ChanelVM({ Name: "Grupa 2", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: false });
+    var chanel8 = new ChanelVM({ Name: "Grupa 3", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: true });
+    var chanel9 = new ChanelVM({ Name: "Grupa 4", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: true });
+    var chanel10 = new ChanelVM({ Name: "Grupa 5", AvatarUri: "Content/Images/sample.jpg", IsOffline: true, AllRead: true });
+    self.GroupChannels.push(chanel6);
+    self.GroupChannels.push(chanel7);
+    self.GroupChannels.push(chanel8);
+    self.GroupChannels.push(chanel9);
+    self.GroupChannels.push(chanel10);
+
+    self.SelectedChanelName("Kontakt 1");
+
+    self.LoggedUser.AvatarUri = "Content/Images/sample.jpg";
+    self.LoggedUser.name = "Kotek Przykladowy";
+    self.LoggedUser.email = "kotek@plotek.pl";
 }
